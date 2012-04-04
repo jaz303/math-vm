443fca1ffbef91aff564c8e9d14f4fb049b56cab
Jason Frame <jason@onehackoranother.com>
2012-04-04 15:57:48 +0100

    reformatting

--- test.c
+++ test.c
@@ -10,11 +10,11 @@ int main(int argc, char *argv[]) {
     
     // input0 * sin(pi / 4)
     inst_t code[5];
-    code[0].opcode      = OP_PUSH;          code[0].operand.value = M_PI / 4;
-    code[1].opcode      = OP_SIN;
-    code[2].opcode      = OP_PUSH_INPUT;    code[2].operand.input_register = 0;
-    code[3].opcode      = OP_MUL;
-    code[4].opcode      = OP_HALT;
+    code[0].opcode      = OP_PUSH       ;   code[0].operand.value = M_PI / 4        ;
+    code[1].opcode      = OP_SIN        ;
+    code[2].opcode      = OP_PUSH_INPUT ;   code[2].operand.input_register = 0      ;
+    code[3].opcode      = OP_MUL        ;
+    code[4].opcode      = OP_HALT       ;
     
     VALUE stack[128];
     
