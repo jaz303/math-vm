26248eff94fc18c18d966a749e2b27e343c75224
Jason Frame <jason@onehackoranother.com>
2012-04-04 15:57:55 +0100

    cycle detection

--- tsort.c
+++ tsort.c
@@ -1,9 +1,17 @@
 #include "tsort.h"
 
-static int tsort_r(graph_t *g, node_id *o, char *visited, node_id n) {
-    if (!visited[n]) {
-        int total_added = 0;
-        visited[n] = 1;
+#define UNVISITED                       0
+#define VISITED_PREVIOUSLY              1
+#define VISITED_FROM_CURRENT_ENDPOINT   2
+
+static int tsort_r(graph_t *g, node_id *o, unsigned char *visited, node_id n) {
+    int total_added = 0;
+    if (visited[n] == VISITED_PREVIOUSLY) {
+        return 0; /* node was already visited from a different endpoint */
+    } else if (visited[n] == VISITED_FROM_CURRENT_ENDPOINT) {
+        return -1; /* cycle! */
+    } else {
+        visited[n] = VISITED_FROM_CURRENT_ENDPOINT;
         int i, num_incoming = g->nodes[n].num_incoming;
         for (i = 0; i < num_incoming; i++) {
             int num_added = tsort_r(g, o, visited, g->nodes[n].incoming[i]);
@@ -11,20 +19,20 @@ static int tsort_r(graph_t *g, node_id *o, char *visited, node_id n) {
             total_added += num_added;
         }
         *o = n;
-        return total_added + 1;
-    } else {
-        return 0;
+        ++total_added;
     }
+    
+    return total_added;
 }
 
 int tsort(graph_t *g, node_id *o) {
     
-    int         i;
-    char        visited[g->num_nodes];
-    node_id     s[g->num_nodes];
+    int             i;
+    unsigned char   visited[g->num_nodes];
+    node_id         s[g->num_nodes];
     
     for (i = 0; i < g->num_nodes; ++i) {
-        visited[i] = 0;
+        visited[i] = UNVISITED;
         s[i] = 0;
     }
     
@@ -42,6 +50,10 @@ int tsort(graph_t *g, node_id *o) {
                 return 0;
             }
             o += nodes_added;
+            int j;
+            for (j = 0; j < g->num_nodes; ++j) {
+                if (visited[j]) visited[j] = VISITED_PREVIOUSLY;
+            }
         }
     }
     
